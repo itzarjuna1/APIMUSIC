76a2e5dc7c50e56a7511dd09ea674ff862f12e19
ARJUN <pachangedinanath@gmail.com>
2025-12-27 01:46:03 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -28,6 +28,6 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ]
+        ],
         
     return buttons
